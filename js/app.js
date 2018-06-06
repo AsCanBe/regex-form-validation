0e59f2c644dc6300c38c99047ef7a21690c484e7
Guy Hunter-Rodwell <ghrodwell@gmail.com>
2018-06-06 19:32:33 +0100

    Basic form validation for username, password, telephone and email inputs, using JavaScript regular expressions.

---
+++ js/app.js
@@ -0,0 +1,55 @@
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const telephoneInput = document.getElementById("telephone");
+const emailInput = document.getElementById("email");
+
+// Validation functions
+function isValidUsername(username) {
+	return /^\w{6,16}$/.test(username);
+}
+
+function isValidPassword(password) {
+	return /[A-Z0-9]/.test(password) &&
+		   /^\w{6,20}$/.test(password);
+}
+
+function isValidTelephone(telephone) {
+	return /^\d{5}\s*\d{3}\s*\d{3}\s*$/.test(telephone);
+}
+
+function isValidEmail(email) {
+	return /^[^@]+@[^@]+\.[a-z]+$/i.test(email);
+}
+
+//Utility functions
+function showOrHideTip(show, element) {
+  if (show) {
+    element.style.display = "inherit";
+  } else {
+    element.style.display = "none";
+  }
+}
+
+function createListener(validator) {
+  return e => {
+    const text = e.target.value;
+    const valid = validator(text);
+    const showTip = text !== "" && !valid;
+    const tooltip = e.target.nextElementSibling;
+    showOrHideTip(showTip, tooltip);
+  };
+}
+
+usernameInput.addEventListener("input", createListener(isValidUsername));
+
+passwordInput.addEventListener("input", createListener(isValidPassword));
+
+telephoneInput.addEventListener("input", createListener(isValidTelephone));
+
+emailInput.addEventListener("input", createListener(isValidEmail));
+
+
+
+
+
+
